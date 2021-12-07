2aca015069db2105832f2898dc58b564e84e24cd
Zafer CÖMERT <zcomert@samsun.edu.tr>
2021-12-07 22:52:43 +0300

    The solutions of the related Lab course.

--- OOP.EFCore.ConsoleApp/Program.cs
+++ OOP.EFCore.ConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using OOP.EFCore.ConsoleApp.DAL;
+using OOP.EFCore.ConsoleApp.Entities;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace OOP.EFCore.ConsoleApp
@@ -8,16 +10,139 @@ namespace OOP.EFCore.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var _context = new BookAppDbContext();
-            
-            var category = _context.Categories
-                            .Where(c => c.CategoryId == 1)
-                            .SingleOrDefault();
-
-            _context.Categories.Remove(category);
-            _context.SaveChanges();
-              
             Console.ReadKey();
         }
+
+        private static void AddBookWithCategoryAndDetails()
+        {
+            using (var _context = new BookAppDbContext())
+            {
+                var book = new Book
+                {
+                    Title = "Database Management",
+                    Price = 400,
+                    Category = _context
+                                .Categories
+                                .OrderBy(c => c.CategoryId)
+                                .FirstOrDefault(),
+                    BookDetail = new BookDetail
+                    {
+                        City = "Samsun",
+                        Country = "Turkey",
+                        ISSN = "123-456-789"
+                    }
+                };
+                _context.Books.Add(book);
+                _context.SaveChanges();
+
+                ListOfBooks();
+            }
+        }
+
+        private static void AddBookWithCategory()
+        {
+            var book = new Book
+            {
+                Title = "Software Engineering",
+                Price = 550,
+                Category = new Category
+                {
+                    CategoryName = "Software"
+                }
+            };
+
+            using (var _context = new BookAppDbContext())
+            {
+                _context.Books.Add(book);
+                _context.SaveChanges();
+
+                ListOfBooks();
+
+                Console.WriteLine(new String('-', 15));
+                _context.Categories
+                    .ToList()
+                    .ForEach(c => Console.WriteLine(c.CategoryName));
+            }
+        }
+
+        private static void DeleteBook()
+        {
+            using (var _context = new BookAppDbContext())
+            {
+                var book = _context
+                    .Books
+                    .OrderBy(b => b.BookId)
+                    .LastOrDefault();
+
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+                ListOfBooks();
+            }
+        }
+
+        private static void UpdateBook()
+        {
+            using (var _context = new BookAppDbContext())
+            {
+                var book = _context
+                    .Books
+                    .OrderBy(b => b.BookId)
+                    .FirstOrDefault();
+
+                book.Title = "Updated book";
+                book.Price = 10;
+
+                _context.SaveChanges();
+
+                ListOfBooks();
+            }
+        }
+
+        private static void ListOfBooks()
+        {
+            var list = new List<Book>();
+            using (var _context = new BookAppDbContext())
+            {
+                list = _context.Books.ToList();
+                foreach (var b in list)
+                {
+                    Console.WriteLine($"{b.BookId,-5} " +
+                        $"{b.Title,-45}" +
+                        $"{b.Price,-5}");
+                }
+            }
+        }
+
+        private static void AddBooks()
+        {
+            var list = new List<Book> {
+                new Book { Title = "EF Core 1", Price =25},
+                new Book { Title = "EF Core 2", Price =125},
+                new Book { Title = "EF Core 3", Price =225},
+                new Book { Title = "EF Core 4", Price =325},
+                new Book { Title = "EF Core 5", Price =425}
+            };
+            using (var _context = new BookAppDbContext())
+            {
+                _context.Books.AddRange(list);
+                _context.SaveChanges();
+            }
+        }
+
+        private static void AddBook()
+        {
+            var book = new Book
+            {
+                Title = "Entity Framework Core",
+                Price = 105
+            };
+
+            using (var _context = new BookAppDbContext())
+            {
+                _context.Books.Add(book);
+                _context.SaveChanges();
+            }
+        }
+
     }
 }
